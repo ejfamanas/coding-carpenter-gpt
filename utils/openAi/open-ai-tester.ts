01c6cadbf412ee6920ef276d7b24ca3aeff579ea
fanman <ejfamanas@gmail.com>
2025-01-18 14:51:21 +0000

    error handling

--- utils/openAi/open-ai-tester.ts
+++ utils/openAi/open-ai-tester.ts
@@ -3,15 +3,21 @@ import OpenAiLoader from "./open-ai-loader";
 export default class OpenAiTester extends OpenAiLoader {
     public async testConnectivity(model: string = "gpt-4o-mini"): Promise<string> {
         const arr: Array<string> = [];
-        const stream = await this.OpenAI.chat.completions.create({
-            model,
-            messages: [{role: "user", content: "Say this is a test"}],
-            stream: true,
-        });
+        try {
+            const stream = await this.OpenAI.chat.completions.create({
+                model,
+                messages: [{role: "user", content: "Say this is a test"}],
+                stream: true,
+            });
 
-        for await (const chunk of stream) {
-            arr.push(chunk.choices[0].delta.content||"");
+            for await (const chunk of stream) {
+                arr.push(chunk.choices[0].delta.content||"");
+            }
+            return Promise.resolve(arr.join(""))
+        } catch (e) {
+            console.log(`${Date.now()}: An error has occurred connecting to OpenAI `${e})
         }
-        return Promise.resolve(arr.join(""))
+
     }
+
 }
